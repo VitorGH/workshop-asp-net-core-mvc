1b4154e1bd27116c0a20c8f9996c8967d413b645
VitorGH <vitorbarrosor@gmail.com>
2025-03-08 14:53:19 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["Message"] = "Sales Web MVC from C# Course";
+        ViewData["Author"] = "Vitor Barrosor Rodrigues";
         return View();
     }
 
